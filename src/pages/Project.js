4cefb8b7ad72d7f8db400f5aa66f4aa3a1fc75cc
donavonelli <donavonelli@yahoo.com>
2021-03-04 14:14:05 -0500

    small tweaks

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,10 +1,4 @@
 import React from 'react'
-import dedster from '../images/dedster.png'
-import makeasip from '../images/makeasip.png'
-import fashion from '../images/fashion.png'
-import giggle from '../images/giggle.png'
-import wayfarer from '../images/wayfarer.png'
-import spades from '../images/spades.png'
 import './Project.css'
 
 const Project = function() {

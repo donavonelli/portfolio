647985cedfda2bdd25adf604d8c77c521758cfc9
donavonelli <donavonelli@yahoo.com>
2021-03-04 14:06:35 -0500

    fixed small bug

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -13,53 +13,6 @@ const Project = function() {
     return (
     <div>
         <h2>My Projects</h2>
-        <div className="slideshow-container">
-            <div className="mySlides fade">
-                <div className="numbertext">1 / 6</div>
-                <img src={dedster} alt='dedster'/>
-                <div className="text">Dedster</div>
-            </div>
-
-            <div className="mySlides fade">
-                <div className="numbertext">2 / 6</div>
-                <img src={makeasip} alt='Make-A-Sip'/>
-                <div className="text">Make-A-Sip</div>
-            </div>
-
-            <div className="mySlides fade">
-                <div className="numbertext">3 / 6</div>
-                <img src={fashion} alt='Your Fashion Career'/>
-                <div className="text">Your Fashion Career</div>
-            </div>
-
-            <div className="mySlides fade">
-                <div className="numbertext">4 / 6</div>
-                <img src={giggle} alt='Giggle Productions'/>
-                <div className="text">Giggle Productions</div>
-            </div>
-
-            <div className="mySlides fade">
-                <div className="numbertext">5 / 6</div>
-                <img src={wayfarer} alt='Wayfarer'/>
-                <div className="text">Wayfarer</div>
-            </div>
-
-            <div className="mySlides fade">
-                <div className="numbertext">6 / 6</div>
-                <img src={spades} alt='Spades'/>
-                <div className="text">Spades</div>
-            </div>
-
-            {/* <a className="prev" onclick="plusSlides(-1)">&#10094;</a>
-            <a className="next" onclick="plusSlides(1)">&#10095;</a> */}
-        </div>
-        <br/>
-
-        <div className='div'>
-        <span className="dot" onclick="currentSlide(1)"></span>
-        <span className="dot" onclick="currentSlide(2)"></span>
-        <span className="dot" onclick="currentSlide(3)"></span>
-        </div>
     </div> 
     )
 }
